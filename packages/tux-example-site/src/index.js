4acbd25d58603c1b3a1defd16f90b71fd5ecfefc
Eirikur Nilsson <eirikur@nilsson.is>
2017-02-10 11:24:30 +0000

    Dynamic redirectUri
    Fixes #5

--- packages/tux-example-site/src/index.js
+++ packages/tux-example-site/src/index.js
@@ -10,7 +10,7 @@ const adapter = createContentfulAdapter({
   space: 'n2difvpz16fj',
   deliveryToken: 'cb34b8fc758364cdbe2b9f73f640ac6ce55311176f9699cf06a0c44325a27208',
   clientId: '7074da10fd8b52237feeccead1462c5c898eaf58a69dc2df08c65b1b306553b6',
-  redirectUri: 'https://localhost:3000/'
+  redirectUri: process.env.PUBLIC_URL || `${location.protocol}//${location.host}/`
 })
 
 ReactDOM.render(
