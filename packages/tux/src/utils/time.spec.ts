9dc60477f443af6bcd5a98b46c98610255fe367a
David Bachmann <dabbi@dabbi.is>
2017-03-01 16:16:43 +0000

    Var name clashing

--- packages/tux/src/utils/time.spec.ts
+++ packages/tux/src/utils/time.spec.ts
@@ -1,23 +1,23 @@
 import { timeSince } from './time'
 
 test('Convert a date', () => {
-  const time = new Date()
-  expect(timeSince(time)).toEqual('0 seconds')
+  const timeNow = new Date()
+  expect(timeSince(timeNow)).toEqual('0 seconds')
 })
 
 test('Return a string when handed a timedate format string', () => {
-  const string = '2017-02-20T22:41:14.565Z'
-  expect(typeof(timeSince(new Date(string)))).toBe('string')
-  expect(timeSince(new Date(string))).not.toBe('NaN seconds')
+  const stringToCheck = '2017-02-20T22:41:14.565Z'
+  expect(typeof(timeSince(new Date(stringToCheck)))).toBe('string')
+  expect(timeSince(new Date(stringToCheck))).not.toBe('NaN seconds')
 })
 
 test('Return a string when handed a readable date string', () => {
-  const string = 'March 1, 2017 22:41:14'
-  expect(typeof(timeSince(new Date(string)))).toBe('string')
-  expect(timeSince(new Date(string))).not.toBe('NaN seconds')
+  const stringToCheck = 'March 1, 2017 22:41:14'
+  expect(typeof(timeSince(new Date(stringToCheck)))).toBe('string')
+  expect(timeSince(new Date(stringToCheck))).not.toBe('NaN seconds')
 })
 
 test('Return the string "NaN seconds" when handed a unsupported string', () => {
-  const string = 'Hey Siri, what time is it?'
-  expect(timeSince(new Date(string))).toBe('NaN seconds')
+  const stringToCheck = 'Hey Siri, what time is it?'
+  expect(timeSince(new Date(stringToCheck))).toBe('NaN seconds')
 })
