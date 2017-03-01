7c8fc5b2df671c2d5a2d35ba021e2ad14e2c4152
David Bachmann <dabbi@dabbi.is>
2017-03-01 16:14:41 +0000

    Accidentally checking typeof

--- packages/tux/src/utils/time.spec.ts
+++ packages/tux/src/utils/time.spec.ts
@@ -8,13 +8,13 @@ test('Convert a date', () => {
 test('Return a string when handed a timedate format string', () => {
   const string = '2017-02-20T22:41:14.565Z'
   expect(typeof(timeSince(new Date(string)))).toBe('string')
-  expect(typeof(timeSince(new Date(string)))).not.toBe('NaN seconds')
+  expect(timeSince(new Date(string))).not.toBe('NaN seconds')
 })
 
 test('Return a string when handed a readable date string', () => {
   const string = 'March 1, 2017 22:41:14'
   expect(typeof(timeSince(new Date(string)))).toBe('string')
-  expect(typeof(timeSince(new Date(string)))).not.toBe('NaN seconds')
+  expect(timeSince(new Date(string))).not.toBe('NaN seconds')
 })
 
 test('Return the string "NaN seconds" when handed a unsupported string', () => {
