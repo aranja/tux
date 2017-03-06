a1b325cc4ccc0f406253dbe8ce49b7013e3fb7e0
David Bachmann <dabbi@dabbi.is>
2017-03-06 15:08:15 +0000

    Datepicker in pop-up window

--- packages/tux/src/styles.tsx
+++ packages/tux/src/styles.tsx
@@ -26,3 +26,13 @@ export const tuxButtonStyles = {
     borderColor: '#188E18',
   }
 }
+
+export const tuxDatePickerStyles = {
+  backgroundColor: '#FFFFFF',
+  backgroundColorOutside: '#FAFAFA',
+  backgroundColorDisabled: '#EFF1F1',
+  backgroundColorSelected: '#3BB172',
+  textColor: '#DCE0E0',
+  textColorSelected: '#FFFFFF',
+  textColorToday: '#3BB172',
+}
