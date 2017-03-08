3f1850f42bc94bcd53ee24346fbfe7b5232ee719
David Bachmann <dabbi@dabbi.is>
2017-03-08 13:27:32 +0000

    Split Bool checkbox into its own component that's used in Bool and Radio

--- packages/tux/src/components/fields/Boolean.tsx
+++ packages/tux/src/components/fields/Boolean.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { tuxColors, tuxInputStyles } from '../../styles'
+import Checkbox from './Checkbox'
 
 export interface State {
   trueOrFalse: boolean | null,
@@ -12,7 +13,7 @@ class Boolean extends Component<any, State> {
 
   componentDidMount() {
     const { value } = this.props
-
+    console.log(value)
     this.setState({
       trueOrFalse: value
     })
@@ -42,34 +43,29 @@ class Boolean extends Component<any, State> {
           className="Boolean is-true"
           htmlFor={trueLabel}>
             {trueLabel}
-          <input
-            className="Boolean-input"
-            id={trueLabel}
-            type="checkbox"
-            checked={trueOrFalse === true}
-            onChange={(event) => {this.onChangeValue(event.target.checked)}}
-          />
-          <div className="Boolean-inputIndicator"></div>
+            <Checkbox
+              id={trueLabel}
+              checked={trueOrFalse === true}
+              onChange={(event) => {this.onChangeValue(event.target.checked)}}
+            />
         </label>
 
         <label
           className="Boolean is-false"
           htmlFor={falseLabel}>
             {falseLabel}
-          <input
-            className="Boolean-input"
-            id={falseLabel}
-            type="checkbox"
-            checked={trueOrFalse === false}
-            onChange={(event) => {this.onChangeValue(!event.target.checked)}}
-          />
-          <div className="Boolean-inputIndicator"></div>
+            <Checkbox
+              id={falseLabel}
+              checked={trueOrFalse === false}
+              onChange={(event) => {this.onChangeValue(event.target.checked)}}
+            />
         </label>
 
       <style jsx>{`
         .Boolean-wrapper {
             margin: 20px 0;
         }
+
         .Boolean {
           cursor: pointer;
           display: block;
@@ -81,14 +77,6 @@ class Boolean extends Component<any, State> {
           user-select: none;
         }
 
-        .Boolean-input {
-          opacity: 0;
-          position: absolute;
-          z-index: -1;
-          box-sizing: border-box;
-          padding: 0;
-        }
-
         .Boolean-inputLabel {
           color: ${tuxInputStyles.labelTextColor};
           font-size: 16px;
@@ -98,41 +86,6 @@ class Boolean extends Component<any, State> {
           text-transform: capitalize;
         }
 
-        .Boolean-inputIndicator {
-          background: ${tuxColors.colorSnow};
-          border: 1px solid ${tuxInputStyles.borderColor};
-          border-radius: 2px;
-          height: 20px;
-          left: 0;
-          position: absolute;
-          top: 2px;
-          width: 20px;
-        }
-
-        .Boolean-inputIndicator::after {
-          border: solid ${tuxColors.colorWhite};
-          border-width: 0 2px 2px 0;
-          content: '';
-          height: 10px;
-          opacity: 0;
-          left: 7px;
-          position: absolute;
-          top: 3px;
-          transform: rotate(45deg) scale(0.5);
-          transform-origin: center;
-          width: 5px;
-        }
-
-        .Boolean-input:checked ~ .Boolean-inputIndicator {
-          background: ${tuxColors.colorGreen};
-          border: 1px solid ${tuxColors.colorGreen};
-        }
-
-        .Boolean-input:checked ~ .Boolean-inputIndicator::after {
-          opacity: 1;
-          transform: rotate(45deg) scale(1);
-          transition: transform 0.1s cubic-bezier(0.17, 0.67, 0.57, 1.17);
-        }
       `}</style>
       </div>
     )
