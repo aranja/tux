beb6e7d192d9b8e741d240e55a9dbf350cc3fe66
Eirikur Nilsson <eirikur@nilsson.is>
2017-03-08 14:52:49 +0000

    JSXHtmlWebpackPlugin rewrite

--- packages/jsx-html-webpack-plugin/index.js
+++ packages/jsx-html-webpack-plugin/index.js
@@ -1,5 +1,9 @@
 'use strict'
-const child_process = require('child_process');
+const vm = require('vm');
+const childCompiler = require('./lib/compiler');
+const prettyError = require('./lib/errors');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 const path = require('path');
 
 class JsxHtmlWebpackPlugin {
@@ -7,10 +11,10 @@ class JsxHtmlWebpackPlugin {
     // Default options
     this.options = Object.assign({
       template: null,
+      cache: true,
       filename: 'index.html',
       manifestFileName: null,
-      babel: null,
-      env: {},
+      showErrors: true,
     }, options);
 
     // Guards.
@@ -20,46 +24,97 @@ class JsxHtmlWebpackPlugin {
   }
 
   apply (compiler) {
-    compiler.plugin('emit', this.buildHtml.bind(this));
-  }
+    let compilationResult = null;
+    let isCompilationCached = false;
+    let previousAssetJson = null;
 
-  buildHtml(compilation, callback) {
-    const filename = this.options.filename;
-    const template = this.options.template;
-    const babel = this.options.babel;
-    const env = Object.assign({}, process.env, this.options.env);
+    compiler.plugin('make', (compilation, callback) => {
+      childCompiler.compileTemplate(this.options.template, compiler.context, this.options.filename, compilation)
+        .then(newResult => {
+          isCompilationCached = compilationResult && newResult.hash === compilationResult.hash;
+          compilationResult = newResult;
+          callback();
+        });
+    });
 
-    // Add dependency to template file so changes to it recompile with --watch.
-    compilation.fileDependencies.unshift(template);
+    compiler.plugin('emit', (compilation, callback) => {
+      // Get assets
+      const assets = this.htmlWebpackPluginAssets(compilation);
 
-    // Collect assets from the build for injecting into the template.
-    const assets = this.htmlWebpackPluginAssets(compilation);
-    const props = Object.assign({}, this.options.props, { assets });
-
-    // Fork a process to run the react template.
-    const child = child_process.fork(path.join(__dirname, 'child.js'), { env });
-    child.send({ babel, template, props });
-
-    child.on('message', (result) => {
-      let html = result.html;
-      if (this.options.minify) {
-        const minify = require('html-minifier').minify;
-        html = minify(html, this.options.minify);
+      // If the template and the assets did not change we don't have to emit the html
+      const assetJson = JSON.stringify(assets);
+      if (isCompilationCached && this.options.cache && assetJson === previousAssetJson) {
+        return callback();
+      } else {
+        previousAssetJson = assetJson;
       }
 
-      // Replace the compilation result with the evaluated html code
-      compilation.assets[filename] = {
-        source: function () {
-          return html;
-        },
-        size: function () {
-          return html.length;
-        }
-      };
-
-      child.kill();
-      callback();
+      Promise.resolve()
+        // Get the actual JSX component.
+        .then(() => {
+          // Allow to use a custom function instead
+          if (this.options.component) {
+            return this.options.component;
+          }
+          // Once everything is compiled, evaluate the jsx component.
+          return this.evaluateCompilationResult(compilationResult.content);
+        })
+        // Render the component.
+        .then(DocumentComponent => {
+          return this.renderComponent(DocumentComponent, assets);
+        })
+        // In case anything went wrong the promise is resolved
+        // with the error message and an error is logged
+        .catch(err => {
+          compilation.errors.push(prettyError(err, compiler.context).toString());
+          // Prevent caching
+          compilationResult.hash = null;
+          return this.options.showErrors ? prettyError(err, compiler.context).toHtml() : 'ERROR';
+        })
+        // Replace the compilation result with the evaluated html code
+        .then(function (html) {
+          compilation.assets[compilationResult.outputName] = {
+            source: function () {
+              return html;
+            },
+            size: function () {
+              return html.length;
+            },
+          };
+        })
+        .then(callback, callback)
     });
+  }
+
+  evaluateCompilationResult(source) {
+    if (!source) {
+      return Promise.reject('The child compilation didn\'t provide a result');
+    }
+
+    // The LibraryTemplatePlugin stores the template result in a local variable.
+    // To extract the result during the evaluation this part has to be removed.
+    source = source.replace('var JSX_HTML_WEBPACK_PLUGIN_RESULT =', '');
+    const vmContext = vm.createContext(Object.assign({JSX_HTML_WEBPACK_PLUGIN: true, require: require}, global));
+    const vmScript = new vm.Script(source, {filename: this.options.template});
+    // Evaluate code and cast to string
+    let newSource;
+    try {
+      newSource = vmScript.runInContext(vmContext);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+    if (typeof newSource === 'object' && newSource.__esModule && newSource.default) {
+      newSource = newSource.default;
+    }
+    return typeof newSource === 'function'
+      ? Promise.resolve(newSource)
+      : Promise.reject('The loader "' + this.options.template + '" didn\'t return a JSX component.');
+  }
+
+  renderComponent(Component, assets) {
+    const props = Object.assign({ assets: assets }, this.options.props);
+    const element = React.createElement(Component, props);
+    return ReactDOMServer.renderToStaticMarkup(element);
   }
 
   // For now, get asset logic from ManifestPlugin.
--- packages/tux-example-site/src/Html.js
+++ packages/tux-example-site/src/Html.js
@@ -6,16 +6,12 @@ import React, { PropTypes } from 'react'
  *
  * To begin the development, run `npm start`.
  * To create a static bundle, use `npm run build`.
- * To create a server bundle, use `npm run server`.
+ * To create a server bundle, use `npm run build-server`.
  *
  * IMPORTANT: This file is compiled and used differently from other modules.
  *
  * -   In development and in static builds, this code is run at build time. It
  *     only receives asset metadata as a prop. Nothing else.
- *
- *     Additionally, it runs in a simple NodeJS + Babel context. So while it can import
- *     other modules, it can't import anything that requires a webpack loader or other
- *     webpack features.
  *
  * -   In server builds, this code evaluates for every request. It may receive
  *     additional route specific props, e.g. a serialized store and meta tags.
