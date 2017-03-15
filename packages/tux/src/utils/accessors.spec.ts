ab2cdc6c14565c4573e8361292ac8dc5751cf345
Bæring Gunnar Steinþórsson <baering90@gmail.com>
2017-03-15 08:22:09 +0000

    Make get handle more levels

--- packages/tux/src/utils/accessors.spec.ts
+++ packages/tux/src/utils/accessors.spec.ts
@@ -10,4 +10,35 @@ describe('the get accessor', () => {
     const result = get(source, 'first')
     expect(result).toEqual(expected)
   })
+
+  it('can fetch two levels deep', () => {
+    const key = 'first.second'
+    const expected = 'nice'
+    const source = {
+      first: {
+        second: expected,
+      },
+      [key]: 'not nice',
+    }
+
+    const result = get(source, key)
+    expect(result).toEqual(expected)
+  })
+
+  it('can fetch four levels deep', () => {
+    const key = 'first.second.third.fourth'
+    const expected = 'nice'
+    const source = {
+      first: {
+        second: {
+          third: {
+            fourth: expected
+          }
+        }
+      }
+    }
+
+    const result = get(source, key)
+    expect(result).toEqual(expected)
+  })
 })
--- packages/tux/src/utils/accessors.ts
+++ packages/tux/src/utils/accessors.ts
@@ -5,6 +5,13 @@
  * @param {String} keyName The lower boundary of the output range
  * @returns {Object} the property value or 'null'.
  */
-export function get(obj: any, keyName: string) {
-  return obj[keyName]
+export function get(obj: any, keyName: string): any {
+  if (keyName.length === 0) {
+    return obj
+  }
+
+  const parts = keyName.split('.')
+  const nextLevel = obj[parts[0]]
+  const restOfKey = parts.slice(1, parts.length).join('.')
+  return get(nextLevel, restOfKey)
 }
