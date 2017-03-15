a55e742ac24f5b40e41f01305ad88bc1f9d85ffc
Eirikur Nilsson <eirikur@nilsson.is>
2017-03-15 02:24:31 +0000

    Clean up extract css magic

--- packages/neutrino-preset-tux/src/extractCss.ts
+++ packages/neutrino-preset-tux/src/extractCss.ts
@@ -2,31 +2,29 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default function extractCss(neutrino: any) {
   const { config } = neutrino
-  const styleRule = config.module.rule('style')
 
-  // We prepend the extract loader by clearing the uses and adding them back in order.
-  // Clone existing uses.
-  const existingUses = styleRule.uses.toConfig()
-
-  styleRule
-    // Clear uses
-    .uses
-      .clear()
-      .end()
-    // Add extract-css loader first.
-    .use('extract-css')
+  // Prepend extract text loader before style loader.
+  prependUse(config.module.rule('style'), (rule: any) =>
+    rule.use('extract-css')
       .loader(require.resolve('extract-text-webpack-plugin/loader'))
       .options({
         omit: 1,
         remove: true,
       })
-      .end()
-    // Then add existing uses again.
-    .merge(existingUses)
+  )
 
   // Add plugin to save extracted css.
   config.plugin('extract-css')
     .use(ExtractTextPlugin, [{
       filename: '[name].[chunkhash].bundle.css',
     }])
+}
+
+// For loaders, the order matters. In this case we want to prepend
+// a loader before existing loaders.
+const prependUse = (rule: any, cb: any) => {
+  const existingUses = rule.uses.entries()
+  rule.uses.clear()
+  cb(rule)
+  rule.uses.merge(existingUses)
 }
