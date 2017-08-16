2e46f7b40b2806c75b3f1ca8134edbc362b96d61
Eirikur Nilsson <eirikur@nilsson.is>
2017-08-16 00:08:16 +0000

    Fix some ts errors

--- packages/tux/src/components/EditModal/EditModal.tsx
+++ packages/tux/src/components/EditModal/EditModal.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { createEditable } from '../Editable/Editable'
 import { EditableProps } from '../../interfaces'
 
-export default createEditable()(({ isEditing, tux, model, ...props }: EditableProps) => (
+export default ({ isEditing, tux, model, ...props }: EditableProps) => (
   <div {...props} />
-))
+)
