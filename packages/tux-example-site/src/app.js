ea769213ae805167b7f1a59b70654356a3db893e
Eirikur Nilsson <eirikur@nilsson.is>
2017-08-23 15:15:51 +0000

    Automatic deploy of tux-example

--- packages/tux-example-site/src/app.js
+++ packages/tux-example-site/src/app.js
@@ -17,12 +17,15 @@ const publicUrl = process.env.PUBLIC_URL
     : `${location.protocol}//${location.host}/`
 
 const adapter = createContentfulAdapter({
+  space: process.env.TUX_CONTENTFUL_SPACE_ID || 'n2difvpz16fj',
   accessToken:
+    process.env.TUX_CONTENTFUL_ACCESS_TOKEN ||
     '61344f3579f7f80450ffc4bc7111624e1035c9588e9680c02519bce08bbbc3ca',
-  clientId: '5be3eeb398764037af2e860d1933bc9518d2f71a72cade411c5f5260e8dd9335',
+  clientId:
+    process.env.TUX_CONTENTFUL_CLIENT_ID ||
+    '5be3eeb398764037af2e860d1933bc9518d2f71a72cade411c5f5260e8dd9335',
   host: 'preview.contentful.com',
   redirectUri: publicUrl,
-  space: 'n2difvpz16fj',
 })
 
 export default createReactChain()
