db47dbb7145c2713fd85f2e7255ee473794ef607
Eirikur Nilsson <eirikur@nilsson.is>
2017-08-28 10:33:12 +0000

    Align error handling with create-react-app

--- packages/neutrino-preset-tux/src/middlewares/web.ts
+++ packages/neutrino-preset-tux/src/middlewares/web.ts
@@ -153,8 +153,19 @@ export default (neutrino: Neutrino) => {
     config
       .devtool('source-map')
       .entry('index')
-      .add(`webpack-dev-server/client?${protocol}://${host}:${port}/`)
-      .add('webpack/hot/dev-server')
+      // Include an alternative client for WebpackDevServer. A client's job is to
+      // connect to WebpackDevServer by a socket and get notified about changes.
+      // When you save a file, the client will either apply hot updates (in case
+      // of CSS changes), or refresh the page (in case of JS changes). When you
+      // make a syntax error, this client will display a syntax error overlay.
+      // Note: instead of the default WebpackDevServer client, we use a custom one
+      // to bring better experience for Create React App users. You can replace
+      // the line below with these two lines if you prefer the stock client:
+      // .add(`webpack-dev-server/client?${protocol}://${host}:${port}/`)
+      // .add('webpack/hot/dev-server')
+      .prepend(require.resolve('react-dev-utils/webpackHotDevClient'))
+      // Errors should be considered fatal in development
+      .add(require.resolve('react-error-overlay'))
   } else {
     neutrino.use(extractStyle)
     neutrino.use(minifyStyle)
