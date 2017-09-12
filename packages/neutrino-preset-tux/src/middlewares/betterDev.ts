f45160473e9a6e625975b7f13331f37c274ff3cb
Eirikur Nilsson <eirikur@nilsson.is>
2017-09-12 11:14:47 +0000

    Watch node_modules to rebuild webpack

--- packages/neutrino-preset-tux/src/middlewares/betterDev.ts
+++ packages/neutrino-preset-tux/src/middlewares/betterDev.ts
@@ -1,6 +1,7 @@
 import { Neutrino } from 'neutrino'
 import path from 'path'
 import errorOverlayMiddleware from 'react-dev-utils/errorOverlayMiddleware'
+import WatchMissingNodeModulesPlugin from 'react-dev-utils/WatchMissingNodeModulesPlugin'
 import { removeEntryPoints } from '../utils'
 import { Options } from '../Options'
 
@@ -20,4 +21,9 @@ export default ({ config }: Neutrino, options: Options) => {
   config.output.devtoolModuleFilenameTemplate((info: any) =>
     path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
   )
+
+  // If you require a missing module and then `npm install` it, you still have
+  // to restart the development server for Webpack to discover it. This plugin
+  // makes the discovery automatic so you don't have to restart.
+  config.plugin('watch-node-modules').use(WatchMissingNodeModulesPlugin)
 }
