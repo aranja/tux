f80f960d067d976ee42a74727768407883792e37
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-11-16 11:39:33 +0000

    Define props that don't exist on the session

--- addons/helmet/test/index.test.tsx
+++ addons/helmet/test/index.test.tsx
@@ -8,6 +8,49 @@ describe('Helmet addon', () => {
     Helmet.canUseDOM = false
   })
 
+  it('should make sure that relevant properties exist on the session', () => {
+    ReactDOMServer.renderToString(
+      <Helmet>
+        <title>Tux Helmet Addon</title>
+      </Helmet>
+    )
+
+    const session = {
+      on(env, fn) {
+        fn(() => {})
+      },
+    }
+
+    helmetAddon()(session)
+
+    expect(typeof session.htmlProps).toBe('object')
+    expect(typeof session.bodyProps).toBe('object')
+    expect(Array.isArray(session.head)).toBeTruthy()
+  })
+
+  it('should not override existing session properties', () => {
+    ReactDOMServer.renderToString(
+      <Helmet>
+        <title>Tux Helmet Addon</title>
+      </Helmet>
+    )
+
+    const session = {
+      htmlProps: { foo: 'bar' },
+      bodyProps: { foo: 'bar' },
+      head: ['test'],
+      on(env, fn) {
+        fn(() => {})
+      },
+    }
+
+    helmetAddon()(session)
+
+    expect(session.htmlProps).toHaveProperty('foo', 'bar')
+    expect(session.bodyProps).toHaveProperty('foo', 'bar')
+    expect(session.head).toHaveLength(2)
+  })
+
   it('renders title tag on the server', () => {
     ReactDOMServer.renderToString(
       <Helmet>
@@ -15,7 +58,7 @@ describe('Helmet addon', () => {
       </Helmet>
     )
 
-    let session = {
+    const session = {
       data: {},
       htmlProps: {},
       bodyProps: {},
